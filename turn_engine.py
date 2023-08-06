872cf07baccfc1e0087a9e0be3dd941871af22aa
1kuceraj <jan.kucera@external.forvia.com>
2023-08-06 20:14:50 +0200

    fix showdown logic

--- turn_engine.py
+++ turn_engine.py
@@ -13,27 +13,27 @@ class TurnEngine(threading.Thread):
         self.running = True
 
     def check_for_updates(self):
-        while True:
-            self.latest_block = blockchain.last_bis_hash()
-            if self.compare_block != self.latest_block:
-                self.turn += 1
-                self.compare_block = self.latest_block
-                update_turn(self.turn)
+        self.latest_block = blockchain.last_bis_hash()
+        if self.compare_block != self.latest_block:
+            self.turn += 1
+            self.compare_block = self.latest_block
+            update_turn(self.turn)
 
-                for username, user_data in load_all_user_data().items():
-                    print("user", username, user_data)
+            for username, user_data in load_all_user_data().items():
+                print("user", username, user_data)
 
-                    if "action_points" in user_data:
-                        updated_action_points = user_data["action_points"] + 20
-                        update_user_data(user=username, updated_values={"action_points": updated_action_points})
+                if "action_points" in user_data:
+                    updated_action_points = user_data["action_points"] + 20
+                    update_user_data(user=username, updated_values={"action_points": updated_action_points})
 
-            print(f"Current turn: {self.turn}")
-            time.sleep(30)
+        print(f"Current turn: {self.turn}")
 
     def run(self):
-        while self.running:
-            self.check_for_updates()
-
+        self.check_for_updates()
+        for _ in range(30):  # For 30 seconds
+            if not self.running:  # If running is set to False, break early
+                break
+            time.sleep(1)  # Sleep for 1 second
     def stop(self):  # Add this method to stop the thread gracefully
         self.running = False
 
