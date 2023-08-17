bc14cd2a2232bb1075876e5325119732dfc01d2f
1kuceraj <jan.kucera@external.forvia.com>
2023-08-17 09:48:25 +0200

    refactor descs

--- descriptions.py
+++ descriptions.py
@@ -1,22 +1,15 @@
 class Descriptions:
-    def get(self, type):
-        if type == "axe":
-            description = "A tool to cut wood with in the forest."
-        elif type == "house":
-            description = "Increases population limit. Cost: 🪵50"
-        elif type == "inn":
-            description = "Restores health"
-        elif type == "farm":
-            description = "Generates food and peasants if housing is available."
-        elif type == "barracks":
-            description = "Two turn peasants into soldiers for 2 food per turn and additional housing."
-        elif type == "sawmill":
-            description = "Collects wood on its own."
-        elif type == "mine":
-            description = "Generates additional resources."
-        elif type == "archery_range":
-            description = "Turns peasants into archers for additional wood and housing."
-        else:
-            description = "Description missing."
+    def __init__(self):
+        self.descriptions_dict = {
+            "axe": "A tool to cut wood with in the forest.",
+            "house": "Increases population limit. Cost: 🪵50",
+            "inn": "Restores health",
+            "farm": "Generates food and peasants if housing is available.",
+            "barracks": "Turns peasants into soldiers for 2 food per turn and additional housing.",
+            "sawmill": "Collects wood on its own.",
+            "mine": "Generates additional resources.",
+            "archery_range": "Turns peasants into archers for additional wood and housing."
+        }
 
-        return description
+    def get(self, type):
+        return self.descriptions_dict.get(type, "Description missing.")
