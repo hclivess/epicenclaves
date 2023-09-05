b02a44db6f7c6ca91259119b14d7be940ec3454d
hclivess <admin@bismuth.cz>
2023-09-05 13:01:16 +0200

    refacotr turn engine

--- turn_engine.py
+++ turn_engine.py
@@ -12,10 +12,8 @@ import string
 
 TEST = 0
 
-
 def fake_hash():
     return "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(36))
-
 
 def interruptible_sleep(seconds, interval=1, stop_condition=None):
     total_time_slept = 0
@@ -41,71 +39,71 @@ class TurnEngine(threading.Thread):
         save_users_from_memory(self.usersdb)
 
     def check_for_updates(self):
-        if TEST:
-            self.latest_block = hashify(fake_hash())
-        else:
-            self.latest_block = blockchain.last_bis_hash()
-
+        self.update_latest_block()
         if self.compare_block != self.latest_block:
             self.save_databases()
-            self.turn += 1
-            self.compare_block = self.latest_block
-            update_turn(self.turn)
+            self.update_turn()
+            self.update_users_data()
+            self.spawn_entities()
+            print(f"Current turn: {self.turn}")
 
-            for username, user_data in self.usersdb.items():
-                updated_values = {}
+    def update_latest_block(self):
+        self.latest_block = hashify(fake_hash()) if TEST else blockchain.last_bis_hash()
 
-                sawmills = 0
-                forests = 0
-                barracks = 0
-                farms = 0
+    def update_turn(self):
+        self.turn += 1
+        self.compare_block = self.latest_block
+        update_turn(self.turn)
 
-                for building_data in user_data.get("construction", {}).values():
-                    if building_data["type"] == "sawmill":
-                        sawmills += building_data.get("size", 1)
-                    elif building_data["type"] == "forest":
-                        forests += building_data.get("size", 1)
-                    elif building_data["type"] == "barracks":
-                        barracks += building_data.get("size", 1)
-                    elif building_data["type"] == "farm":
-                        farms += building_data.get("size", 1)
+    def update_users_data(self):
+        for username, user_data in self.usersdb.items():
+            updated_values = self.calculate_updated_values(user_data)
+            update_user_data(user=username, updated_values=updated_values, user_data_dict=self.usersdb)
 
-                wood_increment = min(sawmills, forests)
-                updated_values["wood"] = user_data["wood"] + wood_increment
+    def calculate_updated_values(self, user_data):
+        building_counts = self.count_buildings(user_data)
+        updated_values = {}
 
-                potential_army_free = min(user_data["food"] // 2, user_data["peasants"], barracks)
-                updated_values["army_free"] = user_data.get("army_free", 0) + potential_army_free
-                updated_values["food"] = user_data["food"] - 2 * potential_army_free
+        wood_increment = min(building_counts['sawmills'], building_counts['forests'])
+        updated_values["wood"] = user_data["wood"] + wood_increment
 
-                potential_peasants_addition = min(farms,
-                                                  user_data["pop_lim"] - (user_data["peasants"] + potential_army_free))
-                updated_values["peasants"] = user_data["peasants"] + potential_peasants_addition
+        potential_army_free = min(user_data["food"] // 2, user_data["peasants"], building_counts['barracks'])
+        updated_values["army_free"] = user_data.get("army_free", 0) + potential_army_free
+        updated_values["food"] = user_data["food"] - 2 * potential_army_free
 
-                update_user_data(user=username, updated_values=updated_values, user_data_dict=self.usersdb)
+        potential_peasants_addition = min(
+            building_counts['farms'], user_data["pop_lim"] - (user_data["peasants"] + potential_army_free))
+        updated_values["peasants"] = user_data["peasants"] + potential_peasants_addition
 
-            spawn_herd(
-                mapdb=self.mapdb,
-                entity_class=Boar,
-                probability=0.5,
-                herd_size=5,
-                max_entities=50,
-            )
+        return updated_values
 
-        print(f"Current turn: {self.turn}")
+    def count_buildings(self, user_data):
+        counts = {'sawmills': 0, 'forests': 0, 'barracks': 0, 'farms': 0}
+        for building_data in user_data.get("construction", {}).values():
+            counts[building_data['type']] += building_data.get('size', 1)
+
+        return counts
+
+    def spawn_entities(self):
+        spawn_herd(
+            mapdb=self.mapdb,
+            entity_class=Boar,
+            probability=0.5,
+            herd_size=5,
+            max_entities=50,
+        )
 
     def run(self):
         while self.running:
             self.check_for_updates()
-            if TEST:
-                interruptible_sleep(1, stop_condition=lambda: not self.running)
-            else:
-                interruptible_sleep(30, stop_condition=lambda: not self.running)
+            sleep_time = 1 if TEST else 30
+            interruptible_sleep(sleep_time, stop_condition=lambda: not self.running)
 
     def stop(self):
         self.running = False
 
 
 if __name__ == "__main__":
-    turn_engine = TurnEngine()
+    turn_engine = TurnEngine({}, {})
     turn_engine.start()
     print("Turn engine started")
