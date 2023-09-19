1eac52a31eb826c276e76054807f0568bda228f4
hclivess <admin@bismuth.cz>
2023-09-19 11:13:08 +0200

    desc

--- descriptions.py
+++ descriptions.py
@@ -6,8 +6,8 @@ class Descriptions:
             "inn": "Allows user to rest and restore health. Cost: 🪵50",
             "farm": "Generates food and peasants if housing is available. Cost: 🪵50",
             "barracks": "Turns peasants into soldiers for 2 food per turn and additional housing. Soldiers are the only deployable unit. Cost: 🪵50",
-            "sawmill": "Passively produces 🪵1 per turn per every level and forest. Cost: 🪵50",
-            "mine": "Passively produces 🪨1 per turn per every level and forest. Cost: 🪵50",
+            "sawmill": "Passively produces 🪵1 per turn per every sawmill level and forest. Cost: 🪵50",
+            "mine": "Passively produces 🪨1 per turn per every mine level and forest. Cost: 🪵50",
             "archery_range": "Allows you to train soldiers into archers. Cost: 🪵50",
             "laboratory": "Turns peasants into researchers to generate research points. Cost: 🪵50",
         }
