38efb129874dcc732e9eff2ed14ecb68ab401a8d
hclivess <admin@bismuth.cz>
2023-09-19 10:22:39 +0200

    desc

--- descriptions.py
+++ descriptions.py
@@ -1,15 +1,15 @@
 class Descriptions:
     def __init__(self):
         self.descriptions_dict = {
-            "axe": "A tool to cut wood with in the forest.",
+            "axe": "A tool to cut wood with in the forest and mine in a mountain.",
             "house": "Increases population limit by 10. Cost: 🪵50",
-            "inn": "Restores health",
-            "farm": "Generates food and peasants if housing is available.",
-            "barracks": "Turns peasants into soldiers for 2 food per turn and additional housing.",
-            "sawmill": "Passively produces 🪵1 per turn per every level and forest.",
-            "mine": "Passively produces 🪨1 per turn per every level and forest.",
-            "archery_range": "Turns peasants into archers for additional wood and housing.",
-            "laboratory": "Turns peasants into researchers to generate research points.",
+            "inn": "Allows user to rest and restore health. Cost: 🪵50",
+            "farm": "Generates food and peasants if housing is available. Cost: 🪵50",
+            "barracks": "Turns peasants into soldiers for 2 food per turn and additional housing. Soldiers are the only deployable unit. Cost: 🪵50",
+            "sawmill": "Passively produces 🪵1 per turn per every level and forest. Cost: 🪵50",
+            "mine": "Passively produces 🪨1 per turn per every level and forest. Cost: 🪵50",
+            "archery_range": "Allows you to train soldiers into archers. Cost: 🪵50",
+            "laboratory": "Turns peasants into researchers to generate research points. Cost: 🪵50",
         }
 
     def get(self, type):
