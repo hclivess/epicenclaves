98006b7d597c31c20ea826074e7ac2fa3e5167ec
hclivess <admin@bismuth.cz>
2024-09-01 12:18:11 +0200

    reset head, hopefully fix drops

--- fight.py
+++ fight.py
@@ -125,11 +125,14 @@ def process_npc_defeat(npc: entities.Enemy, user_data: Dict, user: str, usersdb:
     npc.alive = False
 
     if random.random() < npc.drop_chance:
-        min_item_level = max(1, npc.level - 20)  # Ensure minimum level is at least 1
+        min_item_level = max(1, npc.level - 9)  # Minimum level is at least 1
         max_item_level = npc.level
 
-        # Use logarithmic_level to generate the item level, but enforce the minimum level
-        item_level = max(min_item_level, logarithmic_level(min_item_level, max_item_level))
+        # Ensure item levels follow a logarithmic distribution, but don't fall below min_item_level
+        while True:
+            item_level = logarithmic_level(min_item_level, max_item_level)
+            if item_level >= min_item_level:
+                break
 
         new_item = generate_weapon(min_level=item_level,
                                    max_level=item_level) if random.random() < 0.5 else generate_armor(
@@ -163,6 +166,7 @@ def process_npc_defeat(npc: entities.Enemy, user_data: Dict, user: str, usersdb:
     }
 
     update_user_data(user=user, updated_values=updated_values, user_data_dict=usersdb)
+
 
 
 
