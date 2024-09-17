d1fcf62c3143a83387bdae3854e40e66b1438da1
hclivess <admin@bismuth.cz>
2024-09-18 00:31:34 +0200

    improved train

--- train_sorcery.py
+++ train_sorcery.py
@@ -1,5 +1,5 @@
 import random
-from player import calculate_total_hp
+from player import User, calculate_total_hp
 
 def train_sorcery(user, user_data, usersdb, mapdb):
     exp_cost = 5000
@@ -9,17 +9,19 @@ def train_sorcery(user, user_data, usersdb, mapdb):
     if user_data['exp'] < exp_cost:
         return False, f"You need {exp_cost} exp to train sorcery. You only have {user_data['exp']} exp."
 
+    player = User(user_data['username'], user_data['x_pos'], user_data['y_pos'])  # Create a User object for base_hp
+
     if random.random() < failure_chance:
         user_data['exp'] -= exp_cost
         # Update HP after subtracting exp
-        user_data['hp'] = calculate_total_hp(user_data['base_hp'], user_data['exp'])
+        user_data['hp'] = calculate_total_hp(player.base_hp, user_data['exp'])
         usersdb[user].update(user_data)
         return False, f"Your sorcery training failed. You lost {exp_cost} exp."
 
     user_data['exp'] -= exp_cost
     user_data['sorcery'] = user_data.get('sorcery', 0) + magic_gain
     # Update HP after subtracting exp
-    user_data['hp'] = calculate_total_hp(user_data['base_hp'], user_data['exp'])
+    user_data['hp'] = calculate_total_hp(player.base_hp, user_data['exp'])
 
     usersdb[user].update(user_data)
     return True, f"Your sorcery training was successful! You gained {magic_gain} magic power."
