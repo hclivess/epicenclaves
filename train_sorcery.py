812fb40490618fc77b3d589b0f4c0600d8ce9898
hclivess <admin@bismuth.cz>
2024-09-18 00:26:08 +0200

    improved train

--- train_sorcery.py
+++ train_sorcery.py
@@ -1,5 +1,5 @@
 import random
-
+from player import calculate_total_hp
 
 def train_sorcery(user, user_data, usersdb, mapdb):
     exp_cost = 5000
@@ -11,10 +11,15 @@ def train_sorcery(user, user_data, usersdb, mapdb):
 
     if random.random() < failure_chance:
         user_data['exp'] -= exp_cost
+        # Update HP after subtracting exp
+        user_data['hp'] = calculate_total_hp(user_data['base_hp'], user_data['exp'])
+        usersdb[user].update(user_data)
         return False, f"Your sorcery training failed. You lost {exp_cost} exp."
 
     user_data['exp'] -= exp_cost
     user_data['sorcery'] = user_data.get('sorcery', 0) + magic_gain
+    # Update HP after subtracting exp
+    user_data['hp'] = calculate_total_hp(user_data['base_hp'], user_data['exp'])
 
     usersdb[user].update(user_data)
     return True, f"Your sorcery training was successful! You gained {magic_gain} magic power."
