7246a991fb5e384715964ebe525f0f36d0cf467a
hclivess <admin@bismuth.cz>
2024-09-18 10:26:31 +0200

    mana in bounds on training

--- train_sorcery.py
+++ train_sorcery.py
@@ -1,27 +1,28 @@
 import random
-from player import User, calculate_total_hp
+from player import User, calculate_total_hp, calculate_total_mana
+
 
 def train_sorcery(user, user_data, usersdb, mapdb):
-    exp_cost = 5000
-    magic_gain = 50
-    failure_chance = 0.5
+    EXP_COST = 5000
+    MAGIC_GAIN = 50
+    FAILURE_CHANCE = 0.5
 
-    if user_data['exp'] < exp_cost:
-        return False, f"You need {exp_cost} exp to train sorcery. You only have {user_data['exp']} exp."
+    if user_data['exp'] < EXP_COST:
+        return False, f"You need {EXP_COST} exp to train sorcery. You only have {user_data['exp']} exp."
 
-    player = User(user_data['username'], user_data['x_pos'], user_data['y_pos'])  # Create a User object for base_hp
+    user_data['exp'] -= EXP_COST
 
-    if random.random() < failure_chance:
-        user_data['exp'] -= exp_cost
-        # Update HP after subtracting exp
-        user_data['hp'] = calculate_total_hp(player.base_hp, user_data['exp'])
-        usersdb[user].update(user_data)
-        return False, f"Your sorcery training failed. You lost {exp_cost} exp."
+    player = User(user_data['username'], user_data['x_pos'], user_data['y_pos'])
 
-    user_data['exp'] -= exp_cost
-    user_data['sorcery'] = user_data.get('sorcery', 0) + magic_gain
-    # Update HP after subtracting exp
+    # Recalculate HP and mana after spending EXP
     user_data['hp'] = calculate_total_hp(player.base_hp, user_data['exp'])
+    user_data['mana'] = calculate_total_mana(player.base_hp, user_data['exp'])
+
+    if random.random() < FAILURE_CHANCE:
+        usersdb[user].update(user_data)
+        return False, f"Your sorcery training failed. You lost {EXP_COST} exp."
+
+    user_data['sorcery'] = user_data.get('sorcery', 0) + MAGIC_GAIN
 
     usersdb[user].update(user_data)
-    return True, f"Your sorcery training was successful! You gained {magic_gain} magic power."
+    return True, f"Your sorcery training was successful! You gained {MAGIC_GAIN} magic power."
